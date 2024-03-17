5f35ec7e63a0a9d7226aa39ca265cf5995df9a6e
GhandchiMorteza <mortezaghandchi7@gmail.com>
2024-03-17 20:31:02 +0330

    🍎 basic logic of calculator

--- main.go
+++ main.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import "fmt"
 
+func main() {
+	prices := []float64{10, 20, 30}
+	taxRates := []float64{0, 0.7, 0.1, 0.15}
+
+	result := map[float64][]float64{}
+
+	for _, taxRate := range taxRates {
+		taxIncludedPrices := make([]float64, len(prices))
+		for priceIdx, price := range prices {
+			taxIncludedPrices[priceIdx] = price * (1 + taxRate)
+		}
+		result[taxRate] = taxIncludedPrices
+	}
+
+	fmt.Println(result)
+
+	print(prices, taxRates, result)
 }
